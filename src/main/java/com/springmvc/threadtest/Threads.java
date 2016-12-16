e4c9b80d4d554c4687738c11d0e01cf09ed27ea0
yrpting <rupeng.yan@gmail.com>
2016-12-16 17:12:46 +0800

    ProducerConsumer

--- src/main/java/com/springmvc/threadtest/Threads.java
+++ src/main/java/com/springmvc/threadtest/Threads.java
@@ -7,9 +7,6 @@
 package com.springmvc.threadtest;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,22 +20,27 @@ import org.slf4j.LoggerFactory;
 public class Threads {
 	private static Logger logger = LoggerFactory.getLogger(Threads.class);
 	public static void main(String[] args) {
-		logger.info("main thread {} begin", Thread.currentThread().getName());
-		ExecutorService executor = Executors.newCachedThreadPool();
-		Future<String> future = executor.submit(new Task());
-		//		Future<String> future1 = executor.submit(new Task());
-		executor.shutdown();
-		//		logger.info("main thread {} end", Thread.currentThread().getName());
-		try {
-			Thread.sleep(100);
-			System.out.println(future.cancel(false));
-			logger.info("sub thread : interrupt result:{} {}", future.isCancelled(), future.isDone());
-			//logger.info("sub thread1 :{} result", future1.get());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		logger.info("main thread {} end2", Thread.currentThread().getName());
+		Object a = 2, b = 1;
+		DeadLockA da = new DeadLockA(a, b);
+		DeadLockB db = new DeadLockB(a, b);
+		new Thread(da).start();
+		new Thread(db).start();
+		//		logger.info("main thread {} begin", Thread.currentThread().getName());
+		//		ExecutorService executor = Executors.newCachedThreadPool();
+		//		Future<String> future = executor.submit(new Task());
+		//		//		Future<String> future1 = executor.submit(new Task());
+		//		executor.shutdown();
+		//		//		logger.info("main thread {} end", Thread.currentThread().getName());
+		//		try {
+		//			Thread.sleep(100);
+		//			System.out.println(future.cancel(false));
+		//			logger.info("sub thread : interrupt result:{} {}", future.isCancelled(), future.isDone());
+		//			//logger.info("sub thread1 :{} result", future1.get());
+		//		} catch (Exception e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		}
+		//		logger.info("main thread {} end2", Thread.currentThread().getName());
 
 		//		FutureTask<String> futureTask = new FutureTask<>(new Task());
 		//		//FutureTask<String> futureTask1 = new FutureTask<>(new Task());
@@ -74,4 +76,66 @@ class Task implements Callable<String> {
 		return name;
 	}
 
+}
+
+class DeadLockA implements Runnable {
+	private Object a;
+	private Object b;
+
+	public DeadLockA(Object a, Object b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		synchronized (a) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			synchronized (b) {
+				System.out.println("a,b");
+			}
+		}
+
+	}
+}
+
+class DeadLockB implements Runnable {
+	private Object a;
+	private Object b;
+
+	public DeadLockB(Object a, Object b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		synchronized (b) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			synchronized (a) {
+				System.out.println("b,a");
+			}
+		}
+
+	}
 }
