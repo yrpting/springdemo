25fc7e1cf17e8a92a1ac0912d2a172f3037e6ac6
yrpting <rupeng.yan@gmail.com>
2016-12-20 08:31:31 +0000

    update

--- src/test/java/com/springdemo/test/TestClass.java
+++ src/test/java/com/springdemo/test/TestClass.java
@@ -44,12 +44,15 @@ public class TestClass extends BaseTest {
 	}
 
 	public static void main(String[] args) {
-	    SyncTest s = new SyncTest();
+	    System.out.println(Math.min(Double.MIN_VALUE,0.0d));
+	    System.out.println(1.0d/0.0d);
+	    System.out.println("end");
+	    /*SyncTest s = new SyncTest();
 	    Thread t1= new Thread(s);
 	    Thread t2= new Thread(s);
 	    t1.start();
 	    t2.start();
-	    /*NodeChild c = new NodeChild();
+	    NodeChild c = new NodeChild();
 	    System.out.println(c);
 	    Byte a =  127 ;
 	    Byte b= 127 ;
